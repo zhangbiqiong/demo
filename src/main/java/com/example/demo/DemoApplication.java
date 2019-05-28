c1dbdf9c8a254510fc4e42dba508663d6a628cc7
zhangbiqiong <zhang.biqiong@qq.com>
2019-05-28 13:56:22 +0800

    第二次尝试

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,12 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hello demo zbq");
-		System.out.println("hello demo zbq 0528");
+		System.out.println("hello demo zbq 0528-1-1");
+		System.out.println("hello demo zbq 0528-2-1");
+		System.out.println("hello demo zbq 0528-3-1");
+
+		System.out.println("hello demo zbq 0528-4-1");
+		System.out.println("hello demo zbq 0528-5-1");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
