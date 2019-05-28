d8409e2afaf1e9d76fd10d9fa52c04c1bc33c91f
zhangbiqiong <zhang.biqiong@qq.com>
2019-05-28 15:01:39 +0800

    h2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @SpringBootApplication
 public class DemoApplication {
@@ -40,9 +42,14 @@ public class DemoApplication {
 		private static final String template = "Hello, %s!";
 		private final AtomicLong counter = new AtomicLong();
 
+		@Autowired
+		private UserRepository userRepository;
+
 		@RequestMapping("/greeting")
 		public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-			return new Greeting(counter.incrementAndGet(), String.format(template, name));
+			Optional<DemoUser> o = userRepository.findById(100L);
+			
+			return new Greeting(counter.incrementAndGet(), o.get().getName());
 		}
 	}
 
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+drop table if exists demo_user;
+create table demo_user (id int primary key, username varchar, name varchar, age int,balance int);
