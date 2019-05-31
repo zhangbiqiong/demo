f3f7cfb3e10b4597c54b29d3b8427933a29f7b14
zhangbiqiong <zhang.biqiong@qq.com>
2019-05-31 09:52:31 +0800

    redis

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,20 @@
 package com.example.demo;
 
-import java.math.BigDecimal;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCaching
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -61,6 +62,15 @@ public class DemoApplication {
 			o.setAge(20);
 			userRepository.save(o);
 			return new Greeting(counter.incrementAndGet(), o.getName());
+		}
+
+		@Autowired
+		StringRedisTemplate redisTemplate;
+
+		@RequestMapping("/addRedis")
+		public Greeting addRedis(@RequestParam(value = "name", defaultValue = "World") String name) {
+			redisTemplate.opsForValue().set("name",name);
+			return new Greeting(counter.incrementAndGet(), name);
 		}
 
 	}
